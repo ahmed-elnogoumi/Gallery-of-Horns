164bb2b49f0c0114394b820cc03ebcd7369a2f82
Ahmed Elnogoumi <ahmedelnogoumi@gmail.com>
2024-02-26 21:00:10 -0800

    Added Footer.jsx and App.jsx

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; 2023, Ahmed Elnogoumi</p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <>
+            <header>
+                <h1>Horned Animal Gallery</h1>
+            </header>
+        </>
+    )
+}
+
+export default Header
