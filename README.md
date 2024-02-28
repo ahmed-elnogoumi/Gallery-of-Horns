afd55bb1199b4a8658318617f5db3a026bd48e1f
Ahmed Elnogoumi <ahmedelnogoumi@gmail.com>
2024-02-27 21:20:18 -0800

    added features

--- README.md
+++ README.md
@@ -6,3 +6,26 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+## Lab 02
+
+Number and name of feature: Feature #1, Display Images
+
+Estimate of time needed to complete: 30 minutes
+
+Start time: 8:00pm
+
+Finish time: 8:38pm
+
+Actual time needed to complete: 38 minutes
+
+
+Number and name of feature: Favorite Beasts
+
+Estimate of time needed to complete: 20 minutes
+
+Start time: 9:00pm
+
+Finish time: 9:05pm
+
+Actual time needed to complete: 5 minutes
--- src/HornedBeast.jsx
+++ src/HornedBeast.jsx
@@ -1,9 +1,22 @@
-function HornedBeast() {
+import info from './assets/data.json'
+import React, {useState} from 'react';
+
+function HornedBeast(info) {
+    const [favoritesCount, setFavoritesCount] = useState(0);
+    const handleClick = () => {
+        setFavoritesCount(favoritesCount + 1);
+    }
     return (
         <>
-            <h2>Animal Placeholder Name</h2>
-            <img></img>
-            <p>Placeholder Caption</p>
+            <h2>{info.title}</h2>
+            <img
+                className='Animal-Image'
+                src={info.image_url}
+                alt={info.keyword}
+            />
+            <p>{info.description}</p>
+            <p>Horns: {info.horns}</p>
+            <button onClick={handleClick}>♥</button>
         </>
     )
 }
