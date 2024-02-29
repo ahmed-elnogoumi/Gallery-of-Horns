0ba14091672f7e8922e2474eb1021b1304919659
Ahmed Elnogoumi <ahmedelnogoumi@gmail.com>
2024-02-28 23:48:09 -0800

    working on Gallery projects

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,10 +1,25 @@
 import HornedBeast from './HornedBeast.jsx'
+import Data from './src/data.json'
 
 function Gallery() {
     return (
         <>
-            <HornedBeast></HornedBeast>
-            <HornedBeast></HornedBeast>
+            function App() {
+                return (
+                    <div>
+                    {data.map((item) => (
+                        <HornedBeast
+                        key={item._id}
+                        title={item.title}
+                        image_url={item.image_url}
+                        description={item.description}
+                        keyword={item.keyword}
+                        horns={item.horns}
+                        />
+                    ))}
+    </div>
+  );
+}
         </>
     )
 }
