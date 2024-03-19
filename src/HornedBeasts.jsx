a4249afed9b1b19213f9c411fa40fec6b23be79f
Ahmed Elnogoumi <ahmedelnogoumi@gmail.com>
2024-03-18 21:37:16 -0700

    added modal functionality

--- src/HornedBeasts.jsx
+++ src/HornedBeasts.jsx
@@ -1,27 +1,50 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-
-
+import Image from 'react-bootstrap/Image';
+import Modal from 'react-bootstrap/Modal';
 
 const HornedBeasts = (props) => {
-  console.log(props);
-    
-    return (
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={props.imageUrl} alt="horned beast image" />
-          <Card.Body>
-            <Card.Title>{props.title}</Card.Title>
-            <Card.Text>
-              {props.description}
-            </Card.Text>
-            <p>Number of Horns: {props.horns}</p>
-            <Button variant="primary">  ♥ </Button>
-          </Card.Body>
-        </Card>
-      );
-    }
-  
+  const [likeCount, setLikeCount] = useState(0);
+  const [show, setShow] = useState(false);
+
+  const handleLike = () => {
+    setLikeCount(likeCount + 1);
+  };
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Card style={{ width: '18rem' }}>
+        <Image src={props.imageUrl} alt={props.description} onClick={handleShow} /> 
+        <Card.Body>
+          <Card.Title>{props.title}</Card.Title>
+          <Card.Text>
+            {props.description}
+          </Card.Text>
+          <p>Number of Horns: {props.horns}</p>
+          <p>Likes: {likeCount}</p>
+          <Button variant="primary" onClick={handleLike}>♥</Button>
+        </Card.Body>
+      </Card>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Image Details</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Image src={props.imageUrl} alt={props.title} fluid /> 
+          <p>{props.description}</p>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
 
 export default HornedBeasts;
